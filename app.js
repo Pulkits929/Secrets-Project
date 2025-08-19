eaa4ca4cf23c89098cfa807b34e719a132ef9a59
Pulkit Sharma <spulkit929@gmail.com>
2025-08-19 23:44:33 +0530

    used atlas as database

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/Secrets" , {
-    useNewUrlParser : true
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB Atlas!");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
 });
 const trySchema = new mongoose.Schema({
     name : String,
@@ -22,7 +28,7 @@ const trySchema = new mongoose.Schema({
 const secret = "thisislittlesecret.";
 trySchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
-const item = mongoose.model("Second",trySchema)
+const item = mongoose.model("collection",trySchema)
 
 app.get("/",(req,res)=>{
     res.render("home.ejs");
